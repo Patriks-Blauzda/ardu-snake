2449e8d2a6a72604ac78f6537d4f5d7499cb4ade
Patriks Blauzda <pblauzda@gmail.com>
2022-05-26 16:48:37 +0300

    Collision detection and prepared game states

--- Game.ino
+++ Game.ino
@@ -12,6 +12,7 @@ enum state {
   LOSE
 };
 
+enum state game = GAME;
 
 // vars
 int direction = 3; // movement direction
@@ -19,12 +20,12 @@ int direction = 3; // movement direction
 int x = 3; // snake coordinates
 int y = 4;
 
-int length = 3; // length of the snake
+int length = 10; // length of the snake
 
 // snake movement history array
 byte snaketrail[(128/TILE_SIZE) * (64/TILE_SIZE)][2];
 
-int delayframes = 6; // amount of frames that pass before the snake moves
+int delayframes = 5; // amount of frames that pass before the snake moves
 
 // pushes back all elements and adds new elements to index 0
 // used for saving tail positions
@@ -62,58 +63,70 @@ void loop() {
 
   arduboy.clear();
   
-  arduboy.setCursor(0, 0);
-  
-
-  // delays movement by a set amount of frames while still allowing input
-  if (delayframes > 0) {
-    if (arduboy.pressed(DOWN_BUTTON) && direction != 2) { direction = 1; }
-    if (arduboy.pressed(UP_BUTTON) && direction != 1) { direction = 2; }
-    if (arduboy.pressed(RIGHT_BUTTON) && direction != 4) { direction = 3; }
-    if (arduboy.pressed(LEFT_BUTTON) && direction != 3) { direction = 4; }
+  switch (game){
+    case START:
+      break;
     
-    delayframes -= 1;
     
-  } else {
-    switch (direction) {
-      case 1: // down
-        y += 1;
-        break;
+    case GAME:
+      // delays movement by a set amount of frames while still allowing input
+      if (delayframes > 0) {
+        if (arduboy.pressed(DOWN_BUTTON) && direction != 2) { direction = 1; }
+        if (arduboy.pressed(UP_BUTTON) && direction != 1) { direction = 2; }
+        if (arduboy.pressed(RIGHT_BUTTON) && direction != 4) { direction = 3; }
+        if (arduboy.pressed(LEFT_BUTTON) && direction != 3) { direction = 4; }
         
-      case 2: // up
-        y -= 1;
-        break;
+        delayframes -= 1;
         
-      case 3: // right
-        x += 1;
-        break;
+      } else {
+        switch (direction) {
+          case 1: // down
+            y += 1;
+            break;
+            
+          case 2: // up
+            y -= 1;
+            break;
+            
+          case 3: // right
+            x += 1;
+            break;
+            
+          case 4: // left
+            x -= 1;
+            break;
+        }
         
-      case 4: // left
-        x -= 1;
-        break;
-    }
+        delayframes = 15;
+        
+        snake_pushback(x, y);
+      }
+      
+      
+      if (x > 128/TILE_SIZE - 1) { x = 0; }
+      else if ( x < 0) { x = 128/TILE_SIZE - 1; }
+      
+      if (y > 64/TILE_SIZE - 1) { y = 0; }
+      else if ( y < 0) { y = 64/TILE_SIZE - 1; }
+      
+      
+      // snake tail drawing
+      for (int i = 0; i < length; i++) {
+        if (snaketrail[i + 1][0] == x && snaketrail[i + 1][1] == y){
+          game = LOSE;
+        }
+        
+        arduboy.fillRect(snaketrail[i][0] * TILE_SIZE, snaketrail[i][1] * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+      }
+      
+      
+      arduboy.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+      break;
     
-    delayframes = 15;
     
-    snake_pushback(x, y);
+    case LOSE:
+      break;
   }
-  
-  
-  if (x > 128/TILE_SIZE - 1) { x = 0; }
-  else if ( x < 0) { x = 128/TILE_SIZE - 1; }
-  
-  if (y > 64/TILE_SIZE - 1) { y = 0; }
-  else if ( y < 0) { y = 64/TILE_SIZE - 1; }
-  
-  
-  // snake tail drawing
-  for (int i = 0; i < length; i++) {
-    arduboy.fillRect(snaketrail[i][0] * TILE_SIZE, snaketrail[i][1] * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-  }
-  
-  
-  arduboy.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-  
   
   arduboy.display();
 }
